b53b1a5f72a5e40987b132b73a5123c08fcb3a99
Emir Ozer <emirozer@yandex.com>
2014-02-18 22:34:23 +0200

    drop unnecessary

--- fake2db/fake2db.py
+++ fake2db/fake2db.py
@@ -1,13 +1,15 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='Create test databases that are populated with fake data.')
+parser = argparse.ArgumentParser(description='Create test databases that are '
+                                             '  populated with fake data.')
 
 parser.add_argument('amount of rows', metavar='-r', type=int, nargs='+',
                    help='amount of rows')
+
 parser.add_argument('database pattern', metavar='-p', type=str, nargs='+',
                    help='pattern of the database to be created')
+
 parser.add_argument('target db', metavar='-d', type=int, nargs='+',
                    help='target database / redis ? mongodb ? mysql ? sqlite ?')
 
 args = parser.parse_args()
-print args.accumulate(args.integers)
