a67a6418ff3b935d28e14fe351ac1a724bed4a03
Emir Ozer <emirozer@yandex.com>
2014-02-18 22:36:09 +0200

    bad

--- fake2db/fake2db.py
+++ fake2db/fake2db.py
@@ -3,13 +3,13 @@ import argparse
 parser = argparse.ArgumentParser(description='Create test databases that are '
                                              '  populated with fake data.')
 
-parser.add_argument('amount of rows', metavar='-r', type=int, nargs='+',
+parser.add_argument('amoun_ of_rows', metavar='-r', type=int, nargs='+',
                    help='amount of rows')
 
-parser.add_argument('database pattern', metavar='-p', type=str, nargs='+',
+parser.add_argument('database_pattern', metavar='-p', type=str, nargs='+',
                    help='pattern of the database to be created')
 
-parser.add_argument('target db', metavar='-d', type=int, nargs='+',
+parser.add_argument('target_db', metavar='-d', type=int, nargs='+',
                    help='target database / redis ? mongodb ? mysql ? sqlite ?')
 
 args = parser.parse_args()
