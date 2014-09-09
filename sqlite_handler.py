2173611293ec5e444f4e19111a031294bbe786ee
Emir Ozer <emirozer@yandex.com>
2014-09-09 12:06:03 +0200

    format / docstr

--- sqlite_handler.py
+++ sqlite_handler.py
@@ -3,8 +3,13 @@ import sqlite3
 
 class fake2dbSqliteHander():
     def str_generator(self):
+        '''generates uppercase 6 chars
+        '''
         return ''.join(random.choice(string.ascii_uppercase) for i in range(6))
 
+    def database_creator(self):
+        '''creates a sqlite3 db
+        '''
         try:
             database = self.str_generator() + '.db'
             conn=sqlite3.connect()
