0ae5d9ff3e7dcfe02692d1874a4b9dc635396b8a
Emir Ozer <emirozer@yandex.com>
2014-09-11 17:09:27 +0200

    more structure to sqlite handler

--- fake2db/sqlite_handler.py
+++ fake2db/sqlite_handler.py
@@ -1,8 +1,17 @@
-import logging
 import sqlite3
+import random
+import string
 
+from db_patterns import db_patterns
+from logging import getLogger
 
-class fake2dbSqliteHander():
+logger = getLogger(__name__)
+
+class DbConnException(Exception):
+    """Database Connection or Creation Exception"""
+
+class fake2dbSqliteHandler():
+
     def str_generator(self):
         '''generates uppercase 6 chars
         '''
@@ -11,25 +20,30 @@ class fake2dbSqliteHander():
     def database_caller_creator(self):
         '''creates a sqlite3 db
         '''
+        database = ''
+        
         try:
             database = self.str_generator() + '.db'
             conn=sqlite3.connect(database)
             conn.close()
-            logging.info('Database created and opened succesfully: %s' %database)
+            logger.warning('Database created and opened succesfully: %s' %database)
         except:
-            logging.error('Failed to connect or create database / sqlite3')
+            logger.error('Failed to connect or create database / sqlite3')
+            raise DbConnException
             
         return database
 
-    def data_filler_simple_registration(self, data):
+    def data_filler_simple_registration(self, number_of_rows):
         '''creates and fills the table
-
-        incoming data structure 
-        {'emails' : a_list_of_emails,
-        'passwords': a_list_of_passwords
-        }
-
         '''
+
+        # incoming data structure 
+        # {'emails' : a_list_of_emails,
+        # 'passwords': a_list_of_passwords
+        # }
+        db_patterns_instance = db_patterns()
+        data = db_patterns_instance.simple_registration(number_of_rows)
+
         database = self.database_caller_creator()
         conn = sqlite3.connect(database)
         cursor = conn.cursor()
@@ -40,14 +54,15 @@ class fake2dbSqliteHander():
         ''')
         conn.commit()
 
-        for email, password in data.iteritems()
+        for email, password in data.iteritems():
             try:
                 cursor.execute('''INSERT INTO simple_registration(email, password)
                 VALUES(:email, :password)''',
                                {'email':email, 'password':password})
                 conn.commit()
+                logger.warning('Commit successful after write job!')
             except Exception as e:
-                logging.error(e)
+                logger.error(e)
 
         conn.close()
 
