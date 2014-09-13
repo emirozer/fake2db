a348999abed36f32f459ade81c2d89330a2ff561
Emir Ozer <emirozer@yandex.com>
2014-09-13 13:22:43 +0200

    added db tag + new function to fill detailed registration pattern

--- fake2db/sqlite_handler.py
+++ fake2db/sqlite_handler.py
@@ -18,15 +18,15 @@ class fake2dbSqliteHandler():
         return ''.join(random.choice(string.ascii_uppercase) for i in range(6))
 
     def _rnd_number(self):
-        return random.randint(0, 1000000)
+        return random.randint(0, 100000000)
 
-    def database_caller_creator(self):
+    def database_caller_creator(self, tag):
         '''creates a sqlite3 db
         '''
         database = ''
         
         try:
-            database = self.str_generator() + '.db'
+            database = tag + self.str_generator() + '.db'
             conn=sqlite3.connect(database)
             conn.close()
             logger.warning('Database created and opened succesfully: %s' %database)
@@ -46,7 +46,7 @@ class fake2dbSqliteHandler():
         db_patterns_instance = db_patterns()
         data = db_patterns_instance.simple_registration(number_of_rows)
 
-        database = self.database_caller_creator()
+        database = self.database_caller_creator('simpleRegistration_')
         conn = sqlite3.connect(database)
         cursor = conn.cursor()
         
@@ -59,15 +59,55 @@ class fake2dbSqliteHandler():
         for password in data['passwords']:
             for email in data['emails']:
                 try:
-                    cursor.execute('insert into simple_registration values(?,?,?)',(self._rnd_number(),email,password))
+                    cursor.execute('insert into simple_registration values(?,?,?)',
+                                   (self._rnd_number(),email,password))
                     conn.commit()
                     logger.warning('Commit successful after write job!')
                 except Exception as e:
                     logger.error(e)
 
         conn.close()
+        
 
+    def data_filler_detailed_registration(self, number_of_rows):
+        '''creates and fills the table
+        '''
+        # incoming data structure
+        #fake_data = {'names': list_of_names,
+        #             'lastnames': list_of_lastnames,
+        #             'addresses': list_of_lastnames,
+        #             'phones': list_of_phones,
+        #             'emails': list_of_emails,
+        #             'passwords': list_of_passwords}
 
+        db_patterns_instance = db_patterns()
+        data = db_patterns_instance.detailed_registration(number_of_rows)
 
+        database = self.database_caller_creator('detailedRegistration_')
+        conn = sqlite3.connect(database)
+        cursor = conn.cursor()
+        
+        cursor.execute('''
+        CREATE TABLE detailed_registration(id INTEGER PRIMARY KEY, 
+        email TEXT unique, password TEXT, lastname TEXT,
+        name TEXT, adress TEXT, phone TEXT,)
+        ''')
+        conn.commit()
+        
+        # UGLY AS HELL , TODO: USE ITERTOOLS!!!!!!
+        # TEMPORARY
+        for password in data['passwords']:
+            for email in data['emails']:
+                for name in data['names']:
+                    for lastname in data['lastnames']:
+                        for phone in data['phones']:
+                            for address in data['addresses']:
+                                try:
+                                    cursor.execute('insert into detailed_registration values(?,?,?,?,?,?,?)',(self._rnd_number(), email, password, lastname, name, address, phone))
+                                    conn.commit()
+                                    logger.warning('Commit successful after write job!')
+                                except Exception as e:
+                                    logger.error(e)
 
+        conn.close()
 
