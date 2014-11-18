b749ffb3603642d6aa04f4058a2067d32af08272
Emir Ozer <emirozer@yandex.com>
2014-11-18 15:03:32 +0100

    set up detailed logging frame for main tool

--- fake2db/fake2db.py
+++ fake2db/fake2db.py
@@ -1,13 +1,22 @@
 import sys
 import argparse
+import socket
+import getpass
+import logging
 
 from datetime import date
-from logging import getLogger
-
 from sqlite_handler import fake2dbSqliteHandler
 
-logger = getLogger(__name__)
 
+# Pull the local ip and username for meaningful logging
+username = getpass.getuser()
+local_ip = socket.gethostbyname(socket.gethostname())
+# Set the logger
+FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
+logging.basicConfig(format=FORMAT)
+d = {'clientip': local_ip, 'user': username}
+logger = logging.getLogger('fake2db_logger')
+# --------------------
 
 class InstanciateDBHandlerException(Exception):
     '''An Exception at the instantiation of the handler '''
@@ -32,12 +41,12 @@ parser.add_argument("--db", help="Db type for creation: sqlite, mysql, postgresq
 args = parser.parse_args()
 
 if not args.rows and not args.db:
-    logger.error('Please use with --help argument for usage information!')
+    logger.error('Please use with --help argument for usage information!', extra=d)
 
 if args.rows:
     if args.db:
-        logger.info('arguments found(rows and db), starting faking!!')
-        logger.warning('Rows argument : %s', args.rows)
-        logger.info('DB argument : %s', args.db)
+        logger.info('arguments found(rows and db), starting faking!!', extra=d)
+        logger.warning('Rows argument : %s', args.rows, extra=d)
+        logger.info('DB argument : %s', args.db, extra=d)
         fake_sqlite_handler.data_filler_simple_registration(int(args.rows))
         
