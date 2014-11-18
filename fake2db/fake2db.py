df1ae721095e2e47a95556c7dfd91c3331846fa2
Emir Ozer <emirozer@yandex.com>
2014-11-18 14:56:36 +0100

    lose the cli stuff go to argparse

--- fake2db/fake2db.py
+++ fake2db/fake2db.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 
 from datetime import date
 from logging import getLogger
@@ -6,7 +7,7 @@ from logging import getLogger
 from sqlite_handler import fake2dbSqliteHandler
 
 logger = getLogger(__name__)
-CLI_TAG = 'fake2db >>'
+
 
 class InstanciateDBHandlerException(Exception):
     '''An Exception at the instantiation of the handler '''
@@ -19,49 +20,24 @@ class Fake2Db:
     def help(self):
         print('WORK IN PROGRESS')
 
-while True:
-    user_input = raw_input(CLI_TAG)
 
-    try:
-        fake_sqlite_handler = fake2dbSqliteHandler()
-    except:
-        raise InstanciateDBHandlerException
+try:
+    fake_sqlite_handler = fake2dbSqliteHandler()
+except:
+    raise InstanciateDBHandlerException
 
-    if user_input == 'exit' or user_input == 'quit':
-        Fake2Db().exit()
+parser = argparse.ArgumentParser()
+parser.add_argument("--rows", help="Amount of rows desired per table")
+parser.add_argument("--db", help="Db type for creation: sqlite, mysql, postgresql, mongodb, couchdb, to be expanded")
+args = parser.parse_args()
 
-    elif user_input.startswith('sqlite'):
-        try:
-            # sqlite -simple_registration -3
-            splitted = user_input.split('-')
-            # ['sqlite ', 'simple_registration ', '3']
-            db_pattern = splitted[1].strip()
-            number_of_rows = int(splitted[2])
-        except:
-            print("Your arguments are incorrect. Here's the help document : ")
-            print("                                                         ")
-            Fake2Db().help()
+if not args.rows and not args.db:
+    logger.error('Please use with --help argument for usage information!')
 
-        if db_pattern == 'simple_registration':
-            fake_sqlite_handler.data_filler_simple_registration(number_of_rows)
-        elif db_pattern == 'detailed_registration':
-            fake_sqlite_handler.data_filler_detailed_registration(number_of_rows)
-        elif db_pattern == 'user_agent':
-            fake_sqlite_handler.data_filler_user_agent(number_of_rows)
-        elif db_pattern == 'company':
-            fake_sqlite_handler.data_filler_company(number_of_rows)
-        elif db_pattern == 'customer':
-            logger.error('Customer db creator/ data filler not implemented yet')
-            
-    elif user_input.startswith('postgresql'):
-        pass
-    elif user_input.startswith('mongodb'):
-        pass
-    elif user_input.startswith('couchdb'):
-        pass
-    elif user_input.startswith('mysql'):
-        pass
-    elif user_input == '':
-        pass
-    else:
-        print("You have entered a wrong command or argument , please type help")
+if args.rows:
+    if args.db:
+        logger.info('arguments found(rows and db), starting faking!!')
+        logger.warning('Rows argument : %s', args.rows)
+        logger.info('DB argument : %s', args.db)
+        fake_sqlite_handler.data_filler_simple_registration(int(args.rows))
+        
