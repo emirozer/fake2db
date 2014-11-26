8d41f0a51025a02b1030b5cf9202eb7910038f52
Emir Ozer <emirozer@yandex.com>
2014-11-26 23:22:46 +0100

    added setup.py for package submission

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='fake2db',
+    version='0.1',
+    author='Emir Ozer',
+    author_email='emirozer@yandex.com',
+    url='https://github.com/emirozer/fake2db',
+    description='Generate test databases filled with fake data (current support - sqlite, mysql',
+    long_description=os.path.join(os.path.dirname(__file__), 'README.md'),
+    packages=find_packages(exclude=[]),
+    entry_points={'console_scripts':
+                  ['fake2db = fake2db/fake2db.py']},
+    install_requires=[
+        'faker==0.4.2',
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
