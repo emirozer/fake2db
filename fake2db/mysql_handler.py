cf2960c9f8fbd21ca2994594ba3baa282dffa132
Emir Ozer <emirozer@yandex.com>
2014-12-11 15:56:00 +0100

    rework for mysql

--- fake2db/mysql_handler.py
+++ fake2db/mysql_handler.py
@@ -1,22 +1,10 @@
-import random
-import uuid
-import string
-import logging
-import getpass
-import socket
 import sys
-from db_patterns import DbPatterns
+
+from helpers import fake2db_logger, str_generator, rnd_id_generator
 
 
-# Pull the local ip and username for meaningful logging
-username = getpass.getuser()
-local_ip = socket.gethostbyname(socket.gethostname())
-# Set the logger
-FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
-logging.basicConfig(format=FORMAT)
-d = {'clientip': local_ip, 'user': username}
-logger = logging.getLogger('fake2db_logger')
-# --------------------
+logger, extra_information = fake2db_logger()
+d = extra_information
 
 try:
     import mysql.connector
@@ -26,19 +14,16 @@ except ImportError:
         'you need to get it : http://dev.mysql.com/downloads/connector/python/')
     sys.exit(0)
 
+try:
+    from faker import Factory
+except ImportError:
+    logger.error('faker package not found onto python packages, please run : \
+    pip install -r requirements.txt  \
+    on the root of the project')
+
 
 class Fake2dbMySqlHandler():
-    def str_generator(self):
-        '''generates uppercase 6 chars
-        '''
-        return ''.join(random.choice(string.ascii_uppercase) for i in range(6))
-
-    def _rnd_id_generator(self):
-        '''generates a UUID such as :
-        UUID('dd1098bd-70ac-40ea-80ef-d963f09f95a7')
-        than gets rid of dashes
-        '''
-        return str(uuid.uuid4()).replace('-', '')
+    faker = Factory.create()
 
     def fake2db_mysql_initiator(self, number_of_rows):
         '''Main handler for the operation
@@ -70,7 +55,7 @@ class Fake2dbMySqlHandler():
         conn = None
 
         try:
-            db = 'mysql_' + self.str_generator()
+            db = 'mysql_' + str_generator(self)
             conn = mysql.connector.connect(user='root', host='localhost')
             cursor = conn.cursor()
             cursor.execute('CREATE DATABASE IF NOT EXISTS ' + db)
@@ -127,128 +112,120 @@ class Fake2dbMySqlHandler():
             "  PRIMARY KEY (`id`)"
             ") ENGINE=InnoDB")
 
+        tables['customer'] = (
+            "CREATE TABLE `simple_registration` ("
+            "  `id` varchar(30) NOT NULL,"
+            "  `name` varchar(15) NOT NULL,"
+            "  `lastname` varchar(15) NOT NULL,"
+            "  `address` varchar(20) NOT NULL,"
+            "  `country` varchar(20) NOT NULL,"
+            "  `city` varchar(20) NOT NULL,"
+            "  `registry_date` varchar(20) NOT NULL,"
+            "  `birthdate` varchar(20) NOT NULL,"
+            "  `email` varchar(20) NOT NULL,"
+            "  `phone_number` varchar(20) NOT NULL,"
+            "  `locale` varchar(20) NOT NULL,"
+            "  PRIMARY KEY (`id`)"
+            ") ENGINE=InnoDB")
+
         return tables
 
     def data_filler_simple_registration(self, number_of_rows, cursor, conn):
         '''creates and fills the table with simple regis. information
         '''
-        # incoming data structure 
-        # {'emails' : a_list_of_emails,
-        # 'passwords': a_list_of_passwords
-        # }
-        db_patterns_instance = DbPatterns()
-        data = db_patterns_instance.simple_registration(number_of_rows)
 
-        for password in data['passwords']:
-            for email in data['emails']:
-                try:
-                    simple_registration_payload = ("INSERT INTO simple_registration "
-                                                   "(id, email, password) "
-                                                   "VALUES (%s, %s, %s)")
+        try:
+            for i in range(0, number_of_rows):
+                simple_registration_payload = ("INSERT INTO simple_registration "
+                                               "(id, email, password) "
+                                               "VALUES (%s, %s, %s)")
 
-                    simple_registration_data = (self._rnd_id_generator(), email, password)
-                    cursor.execute(simple_registration_payload, simple_registration_data)
-                    conn.commit()
+                simple_registration_data = (
+                    rnd_id_generator(self), self.faker.safe_email(), self.faker.md5(raw_output=False))
+                cursor.execute(simple_registration_payload, simple_registration_data)
+                conn.commit()
 
-                except Exception as e:
-                    logger.error(e, extra=d)
-
-        logger.warning('simple_registration Commits are successful after write job!', extra=d)
+            logger.warning('simple_registration Commits are successful after write job!', extra=d)
+        except Exception as e:
+            logger.error(e, extra=d)
 
     def data_filler_detailed_registration(self, number_of_rows, cursor, conn):
         '''creates and fills the table with detailed regis. information
         '''
-        # incoming data structure
-        # fake_data = {'names': list_of_names,
-        # 'lastnames': list_of_lastnames,
-        #             'addresses': list_of_lastnames,
-        #             'phones': list_of_phones,
-        #             'emails': list_of_emails,
-        #             'passwords': list_of_passwords}
 
-        db_patterns_instance = DbPatterns()
-        data = db_patterns_instance.detailed_registration(number_of_rows)
+        try:
+            for i in range(0, number_of_rows):
+                detailed_registration_payload = ("INSERT INTO detailed_registration "
+                                                 "(id, email, password, lastname, name,"
+                                                 "adress, phone) "
+                                                 "VALUES (%s, %s, %s, %s, %s, %s, %s)")
+                detailed_registration_data = (
+                    rnd_id_generator(self), self.faker.safe_email(), self.faker.md5(raw_output=False),
+                    self.faker.last_name(), self.faker.name(), self.faker.address(), self.faker.phone_number())
 
-        # UGLY AS HELL , TODO: USE ITERTOOLS!!!!!!
-        # TEMPORARY
-        for password in data['passwords']:
-            for email in data['emails']:
-                for name in data['names']:
-                    for lastname in data['lastnames']:
-                        for phone in data['phones']:
-                            for address in data['addresses']:
-                                try:
+                cursor.execute(detailed_registration_payload, detailed_registration_data)
+                conn.commit()
 
-                                    detailed_registration_payload = ("INSERT INTO detailed_registration "
-                                                                     "(id, email, password, lastname, name,"
-                                                                     "adress, phone) "
-                                                                     "VALUES (%s, %s, %s, %s, %s, %s, %s)")
-                                    detailed_registration_data = (
-                                        self._rnd_id_generator(), email, password, lastname, name, address, phone)
-                                    cursor.execute(detailed_registration_payload, detailed_registration_data)
-                                    conn.commit()
+            logger.warning('detailed_registration Commits are successful after write job!', extra=d)
 
-                                except Exception as e:
-                                    logger.error(e, extra=d)
-
-        logger.warning('detailed_registration Commits are successful after write job!', extra=d)
+        except Exception as e:
+            logger.error(e, extra=d)
 
     def data_filler_user_agent(self, number_of_rows, cursor, conn):
         '''creates and fills the table with user agent data
         '''
-        # incoming data structure
-        # fake_data = {'ips': list_of_ips,
-        # 'countrycodes': list_of_countrycodes,
-        #             'useragents': list_of_useragents}
+        try:
+            for i in range(0, number_of_rows):
+                user_agent_payload = ("INSERT INTO user_agent "
+                                      "(id, ip, countrycode, useragent) "
+                                      "VALUES (%s, %s, %s, %s)")
+                user_agent_data = (rnd_id_generator(self), self.faker.ipv4(), self.faker.country_code(),
+                                   self.faker.user_agent())
+                cursor.execute(user_agent_payload, user_agent_data)
+                conn.commit()
 
-        db_patterns_instance = DbPatterns()
-        data = db_patterns_instance.user_agent(number_of_rows)
-
-        for ip in data['ips']:
-            for countrycode in data['countrycodes']:
-                for useragent in data['useragents']:
-                    try:
-                        user_agent_payload = ("INSERT INTO user_agent "
-                                              "(id, ip, countrycode, useragent) "
-                                              "VALUES (%s, %s, %s, %s)")
-                        user_agent_data = (self._rnd_id_generator(), ip, countrycode, useragent)
-                        cursor.execute(user_agent_payload, user_agent_data)
-                        conn.commit()
-
-                    except Exception as e:
-                        logger.error(e, extra=d)
-
-        logger.warning('user_agent Commits are successful after write job!', extra=d)
+            logger.warning('user_agent Commits are successful after write job!', extra=d)
+        except Exception as e:
+            logger.error(e, extra=d)
 
     def data_filler_company(self, number_of_rows, cursor, conn):
         '''creates and fills the table with company data
         '''
-        # incoming data structure
-        # fake_data = {'names': list_of_names,
-        # 'sdates': list_of_sdates,
-        #             'emails': list_of_emails,
-        #             'domains': list_of_domains,
-        #             'cities': list_of_cities
-        db_patterns_instance = DbPatterns()
-        data = db_patterns_instance.company(number_of_rows)
+        try:
+            for i in range(0, number_of_rows):
+                companies_payload = ("INSERT INTO companies "
+                                     "(id, name, sdate, email, domain, city) "
+                                     "VALUES (%s, %s, %s, %s, %s, %s)")
+                companies_data = (rnd_id_generator(self), self.faker.name(), self.faker.date(pattern="%d-%m-%Y"),
+                                  self.faker.company_email(), self.faker.safe_email(), self.faker.city())
+                cursor.execute(companies_payload, companies_data)
+                conn.commit()
+            logger.warning('companies Commits are successful after write job!', extra=d)
+        except Exception as e:
+            logger.error(e, extra=d)
 
-        for name in data['names']:
-            for sdate in data['sdates']:
-                for email in data['emails']:
-                    for domain in data['domains']:
-                        for city in data['cities']:
-                            try:
-                                companies_payload = ("INSERT INTO companies "
-                                                     "(id, name, sdate, email, domain, city) "
-                                                     "VALUES (%s, %s, %s, %s, %s, %s)")
-                                companies_data = (self._rnd_id_generator(), name, sdate, email, domain, city)
-                                cursor.execute(companies_payload, companies_data)
-                                conn.commit()
+    def data_filler_customer(self, number_of_rows, cursor, conn):
+        '''creates and fills the table with customer
+        '''
 
-                                logger.warning('CLOSING CURSOR!', extra=d)
-                                cursor.close()
+        try:
+            for i in range(0, number_of_rows):
+                customer_payload = ("INSERT INTO detailed_registration "
+                                    "(id, name, lastname, address, country, city, registry_date, birthdate, email, "
+                                    "phone_number, locale)"
+                                    "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+                customer_data = (
+                    rnd_id_generator(self), self.faker.name(), self.faker.last_name(), self.faker.address(),
+                    self.faker.country(), self.faker.city(), self.faker.date(pattern="%d-%m-%Y"),
+                    self.faker.date(pattern="%d-%m-%Y"), self.faker.safe_email(), self.faker.phone_number(),
+                    self.faker.locale())
 
-                            except Exception as e:
-                                logger.error(e, extra=d)
+                cursor.execute(customer_payload, customer_data)
 
-        logger.warning('companies Commits are successful after write job!', extra=d)
+                conn.commit()
+
+            logger.warning('detailed_registration Commits are successful after write job!', extra=d)
+            logger.warning('CLOSING CURSOR!', extra=d)
+            cursor.close()
+        except Exception as e:
+            logger.error(e, extra=d)
