3e2d270bc004076d3f6025a85e91bc23cff19de2
Emir Ozer <emirozer@yandex.com>
2014-12-11 16:18:00 +0100

    wip mongodb

--- fake2db/mongodb_handler.py
+++ fake2db/mongodb_handler.py
@@ -1,23 +1,8 @@
-import random
-import string
-import logging
-import getpass
-import socket
-import sys
-import time
-import subprocess
-from db_patterns import DbPatterns
+from helpers import fake2db_logger, str_generator
 
 
-# Pull the local ip and username for meaningful logging
-username = getpass.getuser()
-local_ip = socket.gethostbyname(socket.gethostname())
-# Set the logger
-FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
-logging.basicConfig(format=FORMAT)
-d = {'clientip': local_ip, 'user': username}
-logger = logging.getLogger('fake2db_logger')
-# --------------------
+logger, extra_information = fake2db_logger()
+d = extra_information
 
 try:
     import pymongo
@@ -26,3 +11,10 @@ except ImportError:
     pip install -r requirements.txt  \
     on the root of the project')
     sys.exit(0)
+
+try:
+    from faker import Factory
+except ImportError:
+    logger.error('faker package not found onto python packages, please run : \
+    pip install -r requirements.txt  \
+    on the root of the project')
