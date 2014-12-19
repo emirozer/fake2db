0eb3b306534e5d00912a5a8f8873636f4c5ca2f4
Emir Ozer <emirozer@yandex.com>
2014-12-19 11:32:19 +0100

    bump version after merging 2 pr's

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='fake2db',
-    version='0.1.4',
+    version='0.1.5',
     author='Emir Ozer',
     author_email='emirozer@yandex.com',
     url='https://github.com/emirozer/fake2db',
