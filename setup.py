131b43a459db13482aec4dafa2e79de8a2d6c260
Emir Ozer <emirozer@yandex.com>
2014-12-30 12:31:43 +0100

    bump setup.py version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='fake2db',
-    version='0.1.7',
+    version='0.1.8',
     author='Emir Ozer',
     author_email='emirozer@yandex.com',
     url='https://github.com/emirozer/fake2db',
