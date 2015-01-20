a9af2616b0842e8898e1133833756cd937f917aa
Nicholas Hrynuik <nhrynuik@gmail.com>
2015-01-19 19:03:16 -0500

    Changed all data filler functions to use hashes instead of key-value pairs.

--- fake2db/redis_handler.py
+++ fake2db/redis_handler.py
@@ -44,10 +44,12 @@ class Fake2dbRedisHandler():
         '''
 
         try:
-            for i in range(0, number_of_rows):
-                pipe.mset({'simple_registration:%s:id' % i: rnd_id_generator(self),
-                           'simple_registration:%s:email' % i: self.faker.safe_email(),
-                           'simple_registration:%s:password' % i: self.faker.md5(raw_output=False)})
+            for i in range(number_of_rows):
+                pipe.hmset('simple_registration:%s' % i, {
+                    'id': rnd_id_generator(self),
+                    'email': self.faker.safe_email(),
+                    'password': self.faker.md5(raw_output=False)
+                })
             pipe.execute()
             logger.warning('simple_registration Commits are successful after write job!', extra=d)
 
@@ -59,14 +61,16 @@ class Fake2dbRedisHandler():
         '''
 
         try:
-            for i in range(0, number_of_rows):
-                pipe.mset({'detailed_registration:%s:id' % i: rnd_id_generator(self),
-                           'detailed_registration:%s:email' % i: self.faker.safe_email(),
-                           'detailed_registration:%s:password' % i: self.faker.md5(raw_output=False),
-                           'detailed_registration:%s:lastname' % i: self.faker.last_name(),
-                           'detailed_registration:%s:name' % i: self.faker.name(),
-                           'detailed_registration:%s:address' % i: self.faker.address(),
-                           'detailed_registration:%s:phone' % i: self.faker.phone_number()})
+            for i in range(number_of_rows):
+                pipe.hmset('detailed_registration:%s' % i, {
+                    'id': rnd_id_generator(self),
+                    'email': self.faker.safe_email(),
+                    'password': self.faker.md5(raw_output=False),
+                    'lastname': self.faker.last_name(),
+                    'name': self.faker.name(),
+                    'address': self.faker.address(),
+                    'phone': self.faker.phone_number()
+                })
             pipe.execute()
             logger.warning('detailed_registration Commits are successful after write job!', extra=d)
 
@@ -78,11 +82,13 @@ class Fake2dbRedisHandler():
         '''
 
         try:
-            for i in range(0, number_of_rows):
-                pipe.mset({'user_agent:%s:id' % i: rnd_id_generator(self),
-                           'user_agent:%s:ip' % i: self.faker.ipv4(),
-                           'user_agent:%s:countrycode' % i: self.faker.country_code(),
-                           'user_agent:%s:useragent' % i: self.faker.user_agent()})
+            for i in range(number_of_rows):
+                pipe.hmset('user_agent:%s' % i, {
+                    'id': rnd_id_generator(self),
+                    'ip': self.faker.ipv4(),
+                    'countrycode': self.faker.country_code(),
+                    'useragent': self.faker.user_agent()
+                })
             pipe.execute()
             logger.warning('user_agent Commits are successful after write job!', extra=d)
 
@@ -94,13 +100,15 @@ class Fake2dbRedisHandler():
         '''
 
         try:
-            for i in range(0, number_of_rows):
-                pipe.mset({'company:%s:id' % i: rnd_id_generator(self),
-                           'company:%s:name' % i: self.faker.company(),
-                           'company:%s:date' % i: self.faker.date(pattern="%d-%m-%Y"),
-                           'company:%s:email' % i: self.faker.safe_email(),
-                           'company:%s:domain' % i: self.faker.safe_email(),
-                           'company:%s:city' % i: self.faker.city()})
+            for i in range(number_of_rows):
+                pipe.hmset('company:%s' % i, {
+                    'id': rnd_id_generator(self),
+                    'name': self.faker.company(),
+                    'date': self.faker.date(pattern="%d-%m-%Y"),
+                    'email': self.faker.safe_email(),
+                    'domain': self.faker.safe_email(),
+                    'city': self.faker.city()
+                })
             pipe.execute()
             logger.warning('companies Commits are successful after write job!', extra=d)
 
@@ -112,18 +120,20 @@ class Fake2dbRedisHandler():
         '''
 
         try:
-            for i in range(0, number_of_rows):
-                pipe.mset({'customer:%s:id' % i: rnd_id_generator(self),
-                           'customer:%s:name' % i: self.faker.name(),
-                           'customer:%s:lastname' % i: self.faker.last_name(),
-                           'customer:%s:address' % i: self.faker.address(),
-                           'customer:%s:country' % i: self.faker.country(),
-                           'customer:%s:city' % i: self.faker.city(),
-                           'customer:%s:registry_date' % i: self.faker.date(pattern="%d-%m-%Y"),
-                           'customer:%s:birthdate' % i: self.faker.date(pattern="%d-%m-%Y"),
-                           'customer:%s:email' % i: self.faker.safe_email(),
-                           'customer:%s:phone_number' % i: self.faker.phone_number(),
-                           'customer:%s:locale' % i: self.faker.locale()})
+            for i in range(number_of_rows):
+                pipe.hmset('customer:%s' % i, {
+                    'id': rnd_id_generator(self),
+                    'name': self.faker.name(),
+                    'lastname': self.faker.last_name(),
+                    'address': self.faker.address(),
+                    'country': self.faker.country(),
+                    'city': self.faker.city(),
+                    'registry_date': self.faker.date(pattern="%d-%m-%Y"),
+                    'birthdate': self.faker.date(pattern="%d-%m-%Y"),
+                    'email': self.faker.safe_email(),
+                    'phone_number': self.faker.phone_number(),
+                    'locale': self.faker.locale()
+                })
             pipe.execute()
             logger.warning('customer Commits are successful after write job!', extra=d)
 
