88bb699807043ebeeae9619f739d89679c6a0c4e
Emir Ozer <emirozer@yandex.com>
2015-01-22 23:45:07 +0100

    bump version for redis addition

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='fake2db',
-    version='0.2.1',
+    version='0.2.2',
     author='Emir Ozer',
     author_email='emirozer@yandex.com',
     url='https://github.com/emirozer/fake2db',
