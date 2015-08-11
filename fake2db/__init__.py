379cccf75354341ad8ad870af5f3862337603a77
Emir Ozer <emirozer@yandex.com>
2015-08-11 21:29:16 +0200

    bump v in init

--- fake2db/__init__.py
+++ fake2db/__init__.py
@@ -8,6 +8,6 @@ fake2db
 """
 
 __title__ = 'fake2db'
-__version__ = '0.2.2'
+__version__ = '0.3.0'
 __author__ = 'Emir Ozer'
-__license__ = 'GNU General Public License'
+__license__ = 'GNU General Public License v2'
