663b5dbc5d7a2ec43335c83a1cc99750c89967f2
Emir Ozer <emirozer@yandex.com>
2015-12-03 23:38:48 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import os
 
 setup(
     name='fake2db',
-    version='0.4.1',
+    version='0.5.0',
     author='Emir Ozer',
     author_email='emirozer@yandex.com',
     url='https://github.com/emirozer/fake2db',
     description=
-    'Generate test databases filled with fake data (current support - sqlite, mysql, postgresql, mongodb, redis, couchdb)',
+    'Generate test databases filled with fake data (NOW CUSTOM SCHEMA CREATION SUPPORTED)(current support - sqlite, mysql, postgresql, mongodb, redis, couchdb)',
     long_description=os.path.join(os.path.dirname(__file__), 'README.md'),
     packages=find_packages(exclude=[]),
     entry_points={'console_scripts': ['fake2db = fake2db.fake2db:main']},
