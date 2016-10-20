6d9de676c42b99779749510495d985db5d5ccf3f
Emir Ozer <emirozer@yandex.com>
2016-10-20 14:04:00 +0200

    increase version after new release

--- fake2db/__init__.py
+++ fake2db/__init__.py
@@ -6,6 +6,6 @@ fake2db
 """
 
 __title__ = 'fake2db'
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 __author__ = 'Emir Ozer'
 __license__ = 'GNU General Public License v2'
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='fake2db',
-    version='0.5.0',
+    version='0.5.1',
     author='Emir Ozer',
     author_email='emirozer@yandex.com',
     url='https://github.com/emirozer/fake2db',
